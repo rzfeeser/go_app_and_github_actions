511639425c85e4196832bd71a2f33a11b174387e
RZFeeser <rzfeeser@users.noreply.github.com>
2025-03-19 14:05:30 -0400

    Update main.go

--- main.go
+++ main.go
@@ -2,8 +2,12 @@
 // Click here and start typing.
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	)
 
 func main() {
 	fmt.Println("Hello, Russell Zachary Feeser of https://rzfeeser.com and https://iris7.com")
+        fmt.Println("Attempting to print the ENV secret from GitHub repo; value of MYTESTENV: ", os.Getenv("MYTESTENV")) 
 }
